590c15637be4abfc103a5ab0e464dc64c29350a6
GeovanniOj <geovannirock@gmail.com>
2019-10-05 15:13:26 -0300

    Addiction of custom layout to set 2 columns for portrait device orientation and 3 columns for landscape orientation

---
+++ NewsApp/NewsApp/View/MainScreen/Cell/HeaderCell.swift
@@ -0,0 +1,9 @@
+//
+//  HeaderCell.swift
+//  NewsApp
+//
+//  Created by Geovanni Oliveira de Jesus on 02/10/19.
+//  Copyright © 2019 Geovanni Oliveira de Jesus. All rights reserved.
+//
+
+import Foundation
